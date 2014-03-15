23690d33bdcccc62970a178cd52c7f06bdcfabf1
Johaned <johan.tique@codescrum.com>
2014-03-15 14:34:59 -0500

    update changeScene in Dashboard

--- src/org/university/unicauca/tdi/scene/Dashboard.java
+++ src/org/university/unicauca/tdi/scene/Dashboard.java
@@ -218,7 +218,7 @@ public class Dashboard extends HComponent implements Scene, HKeyListener, HActio
 			change_scene("Vote");
 			break;
 		case 'C':
-			
+			change_scene("Results");
 			break;
 		case 'E':
 			cleaner();
