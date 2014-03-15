118edee9013c96750183c24de56362de967a2776
Johaned <johan.tique@codescrum.com>
2014-03-14 22:36:55 -0500

    add core class structure (Xlet)

---
+++ src/org/university/unicauca/tdi/app/VotingCore.java
@@ -0,0 +1,29 @@
+package org.university.unicauca.tdi.app;
+
+import javax.tv.xlet.Xlet;
+import javax.tv.xlet.XletContext;
+import javax.tv.xlet.XletStateChangeException;
+
+public class VotingCore implements Xlet{
+
+	public void destroyXlet(boolean arg0) throws XletStateChangeException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void initXlet(XletContext arg0) throws XletStateChangeException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void pauseXlet() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void startXlet() throws XletStateChangeException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
