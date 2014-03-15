e38c1637e6300ad0302968381a8909f46173fa95
Johaned <johan.tique@codescrum.com>
2014-03-15 11:58:13 -0500

    add Scene interface to wrap some useful helpers

--- src/org/university/unicauca/tdi/model/Scene.java
+++ src/org/university/unicauca/tdi/model/Scene.java
@@ -3,14 +3,14 @@ package org.university.unicauca.tdi.model;
 import org.university.unicauca.tdi.app.VotingCore;
 
 public interface Scene {
-	public final int VK_ROJO=403;
-	public final int VK_VERDE=404;
-	public final int VK_AMARILLO=405;
-	public final int VK_AZUL=406;
-	public final int VK_IZQ=37;
-	public final int VK_DER=39;
-	public final int VK_ARR=38;
-	public final int VK_ABA=40;
+	public final int VK_RED=403;
+	public final int VK_GREEN=404;
+	public final int VK_YELLOW=405;
+	public final int VK_BLUE=406;
+	public final int VK_LEFT=37;
+	public final int VK_RIGHT=39;
+	public final int VK_UP=38;
+	public final int VK_DOWN=40;
 	public final int VK_OK=10;
 	
 	public abstract void initializer(VotingCore appXlet);
